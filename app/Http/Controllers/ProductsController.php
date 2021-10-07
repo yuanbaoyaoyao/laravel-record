d59f9804d7e8973bfb8dd06e2afbac867487fd78
yuanbaoyaoyao <yuanbaoyaoyao@qq.com>
2021-10-07 18:52:18 +0800

    异常处理

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -37,7 +38,7 @@ class ProductsController extends Controller
     public function show(Product $product, Request $request)
     {
         if (!$product->in_warehouse) {
-            throw new \Exception('耗材未在库');
+            throw new InvalidRequestException('商品未上架');
         }
 
         return view('products.show', ['product' => $product]);
