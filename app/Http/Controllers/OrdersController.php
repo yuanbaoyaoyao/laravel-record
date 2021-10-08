dead9b3443bb9b5b2276d36e43355afbc6387ade
yuanbaoyaoyao <yuanbaoyaoyao@qq.com>
2021-10-08 14:08:11 +0800

    用户界面 - 需求单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\OrderRequest;
+use Illuminate\Http\Request;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
 use App\Models\Order;
@@ -13,6 +14,19 @@ use App\Exceptions\InvalidRequestException;
 
 class OrdersController extends Controller
 {
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
+    }
+
     public function store(OrderRequest $request)
     {
         $user  = $request->user();
