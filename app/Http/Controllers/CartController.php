81e80e1b12cbc33c660d4656fe2327d5d36aabfd
yuanbaoyaoyao <yuanbaoyaoyao@qq.com>
2021-10-08 18:43:03 +0800

    封装业务代码

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\AddCartRequest;
-use App\Models\CartItem;
 use App\Models\ProductSku;
-use App\Models\UserAddress;
-
+use App\Services\CartService;
 
 class CartController extends Controller
 {
+    protected $cartService;
+
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
 
     public function index(Request $request)
     {
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $cartItems = $this->cartService->get();
         $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
 
         return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
@@ -22,29 +26,14 @@ class CartController extends Controller
 
     public function add(AddCartRequest $request)
     {
-        $user   = $request->user();
-        $skuId  = $request->input('sku_id');
-        $amount = $request->input('amount');
-
-        if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-
-            $cart->update([
-                'amount' => $cart->amount + $amount,
-            ]);
-        } else {
-
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
+        $this->cartService->add($request->input('sku_id'), $request->input('amount'));
 
         return [];
     }
 
     public function remove(ProductSku $sku, Request $request)
     {
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        $this->cartService->remove($sku->id);
 
         return [];
     }
---
+++ app/Services/OrderService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Models\UserAddress;
+use App\Models\Order;
+use App\Models\ProductSku;
+use App\Exceptions\InvalidRequestException;
+use App\Jobs\CloseOrder;
+use Carbon\Carbon;
+
+class OrderService
+{
+    public function store(User $user, UserAddress $address, $remark, $items)
+    {
+        // 开启一个数据库事务
+        $order = \DB::transaction(function () use ($user, $address, $remark, $items) {
+            // 更新此地址的最后使用时间
+            $address->update(['last_used_at' => Carbon::now()]);
+            // 创建一个订单
+            $order   = new Order([
+                'address'      => [ // 将地址信息放入订单中
+                    'address'       => $address->full_address,
+                    'user'          => $address->user,
+                    'contact_phone' => $address->contact_phone,
+                ],
+                'remark'       => $remark,
+            ]);
+            // 订单关联到当前用户
+            $order->user()->associate($user);
+            // 写入数据库
+            $order->save();
+
+            // 遍历用户提交的 SKU
+            foreach ($items as $data) {
+                $sku  = ProductSku::find($data['sku_id']);
+                // 创建一个 OrderItem 并直接与当前订单关联
+                $item = $order->items()->make([
+                    'amount' => $data['amount'],
+                ]);
+                $item->product()->associate($sku->product_id);
+                $item->productSku()->associate($sku);
+                $item->save();
+                if ($sku->decreaseStock($data['amount']) <= 0) {
+                    throw new InvalidRequestException('该耗材库存不足');
+                }
+            }
+
+            // 将下单的商品从购物车中移除
+            $skuIds = collect($items)->pluck('sku_id')->all();
+            app(CartService::class)->remove($skuIds);
+
+            return $order;
+        });
+
+        // 这里我们直接使用 dispatch 函数
+        dispatch(new CloseOrder($order, config('app.order_ttl')));
+
+        return $order;
+    }
+}
